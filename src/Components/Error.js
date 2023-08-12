ed9d7c0c89c6fb7863e586c0ed6d16131d1b5fa5
inderjeetkour1211 <129959168+inderjeetkour1211@users.noreply.github.com>
2023-08-12 12:36:51 +0530

    app updated

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+
 const Error = () => {
-    const err = useRouteError();
-    const { status, statusText } = err;
-    return (
-        <div>
-            <h1>Oops! Something went wrong</h1>
-            <p>
-                {status} : {statusText}
-            </p>
-        </div>
-    );
+  const err = useRouteError();
+  const { status, statusText } = err;
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <p className="text-red-600 text-5xl font-bold">
+        {status} !
+      </p> 
+      <h2 className="text-xl font-semibold text-red-600 my-3">
+        Oops! Something went wrong
+      </h2>
+     
+      <p className="text-red-600 text-xl font-semibold">
+        {statusText}
+      </p>
+    </div>
+  );
 };
 
 export default Error;
